677f47610651269d1536e290685c2728cfe96b2c
Saladin Muhamad <46558837+SaladinIART@users.noreply.github.com>
2020-12-06 07:19:28 +0800

    OpenCV Region of Interest
    Lecture playlist number 19-22 from toptechboy.com

---
+++ openCV7_ROI_Exercise.py
@@ -0,0 +1,45 @@
+import cv2
+print(cv2.__version__)
+dispW=640
+dispH=480
+flip=2
+
+#Uncomment These next Two Line for Pi Camera
+camSet='nvarguscamerasrc !  video/x-raw(memory:NVMM), width=3264, height=2464, format=NV12, framerate=21/1 ! nvvidconv flip-method='+str(flip)+' ! video/x-raw, width='+str(dispW)+', height='+str(dispH)+', format=BGRx ! videoconvert ! video/x-raw, format=BGR ! appsink'
+cam= cv2.VideoCapture(camSet)
+
+dispW=int(cam.get(cv2.CAP_PROP_FRAME_WIDTH))
+dispH=int(cam.get(cv2.CAP_PROP_FRAME_HEIGHT))
+#declare size of BOX
+BW=int(.25*dispW)
+BH=int(.15*dispH)
+#starting position of box
+posX=10
+posY=270
+dx=2
+dy=2 
+
+#Or, if you have a WEB cam, uncomment the next line
+#(If it does not work, try setting to '1' instead of '0')
+#cam=cv2.VideoCapture(0)
+while True:
+    ret, frame = cam.read()
+    #roi=frame[200:270+255,210:215+213].copy() #[initial row:final row,initial column:final column]
+    roi=frame[posY:posY+BH,posX:posX+BW].copy()
+    frame=cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
+    frame=cv2.cvtColor(frame,cv2.COLOR_GRAY2BGR)
+    frame[posY:posY+BH,posX:posX+BW]=roi
+    cv2.rectangle(frame,(posX,posY),(posX+BW,posY+BH),(255,0,0),3)
+    cv2.imshow('nanoCam',frame)
+    cv2.moveWindow('nanoCam',0,0)
+    posX=posX+dx
+    posY=posY+dy
+
+    if posX<=0 or posX+BW>=dispW:
+        dx=dx*(-1)
+    if posY<=0 or posY+BH>=dispH:
+        dy=dy*(-1)
+    if cv2.waitKey(1)==ord('q'):
+        break
+cam.release()
+cv2.destroyAllWindows()
